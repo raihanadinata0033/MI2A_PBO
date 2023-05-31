36f8654fcdc72fbf62c5b2312c6679687ce5421c
raihanadinata0033 <114200662+raihanadinata0033@users.noreply.github.com>
2023-05-31 12:41:12 +0700

    Add files via upload

---
+++ src/mi_2a/BankSwing/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankSwing;
+import BankSwing.TransferModel;
+
+/**
+ *
+ * @author ASPIRE 4752
+ */
+public class InformasiSaldoModel {
+    String saldo="1500000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/mi_2a/BankSwing/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankSwing;
+
+/**
+ *
+ * @author ASPIRE 4752
+ */
+public class PinModel {
+   
+    String pin = "080103";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
